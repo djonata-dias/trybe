b67d930c18d9324a6bf3dd6c4e8aa111634ad8b4
djonata-dias <diasdjonata@gmail.com>
2020-05-05 22:38:48 -0300

    exercicio 2 e arquivos vazios dos proximos

---
+++ exercises/8_3/exercicio2.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+
+function myIndexOf(arr, item) {
+  let searchedIndex = -1;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item === arr[i]) {
+      searchedIndex = i;
+    }
+  }
+  return searchedIndex;
+}
+
+// implemente seus testes aqui
+
+// Teste se a chamada myIndexOf([1, 2, 3, 4], 3) retorna o valor esperado
+// Teste se a chamada myIndexOf([1, 2, 3, 4], 5) retorna o valor esperado
+
+assert.deepEqual(myIndexOf([1, 2, 3, 4], 3), 3)
