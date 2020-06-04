6974f6caa16608c4877c8557378bb3f66cc12e87
djonata-dias <diasdjonata@gmail.com>
2020-06-03 22:45:03 -0300

    criando botões para mudar o pokem após filtrar por tipo de elemento

--- exercises/12_1/my-pokedex/src/App.js
+++ exercises/12_1/my-pokedex/src/App.js
@@ -8,37 +8,64 @@ class Card extends React.Component {
     return (
       data.filter(({ type }) => [type, 'All'].includes(filter))
         .map(({ id, name, type, averageWeight, image }) => (
-          <li key={name}>
+          <li key={id} className="hidden-card">
             <div>
               <img src={image} alt={name} />
             </div>
             <div>
-        {id} - {name} type: {type} averageWeight: {averageWeight.value} {averageWeight.measurementUnit}
+              {id} - {name} type: {type} averageWeight: {averageWeight.value} {averageWeight.measurementUnit}
             </div>
           </li>
-       ))
+        ))
 
     )
-    
+
   }
 }
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = ({ type: 'All' })
+    this.state = ({ type: 'All', indexA: 0, indexB: 0 })
+  }
+  setFilterType = event => {
+   this.setState({ type: event.target.value === '' ? 'All' : event.target.value })
   }
 
-  serFilterType = event => {
-    this.setState({
-      type: event.target.value === '' ? 'All' : event.target.value
-    })
+  setNext = async () => {
+    const li = document.getElementsByTagName('li');
+    const i = this.state.indexA
+    let length = (li.length  < 9 ) ? 1 : li.length - 1;
+    console.log(`length: ${length}, i: ${i}, li.length: ${li.length}`);
+    if(length !== 1){
+    if (i < length ) {
+      await this.setState({ indexA: i + 1 }) 
+    } else {
+      await this.setState({ indexA: 0})
+    }}
+    for (let z = 0; z < li.length; z++) { li[z].classList.remove('card') }
+    li[i].classList.add('card')
+
+  }
+
+  setPreviously = async () => {
+    const li = document.getElementsByTagName('li');
+    const i = this.state.indexB;
+    let length = (li.length - 1 < 1) ? 1 : li.length - 1;
+    console.log(`length: ${length}, i: ${i}, li.length: ${li.length}`);
+
+    if (i > 0) {
+      await this.setState({ indexB: i - 1 }) 
+    } else {
+      await this.setState({ indexB: length})
+    }    for (let z = 0; z < li.length; z++) { li[z].classList.remove('card') }
+    li[i].classList.add('card')
   }
   render() {
     return (
       <div className="App">
         <div className="pokedex">
-          <select onChange={this.serFilterType} type='text'>
+          <select className="element-type" value={this.state.type} onChange={this.setFilterType}>
             <option value="Eletric" >Eletric</option>
             <option value="Fire">Fire</option>
             <option value="Bug">Bug</option>
@@ -46,13 +73,15 @@ class App extends React.Component {
             <option value="Psychic">Psychic</option>
             <option value="Normal">Normal</option>
             <option value="Dragon">Dragon</option>
-            <option value="All" selected="selected">All</option>
+            <option value="All">All</option>
           </select>
+          <div className="pokedex-screen">
+            <Card filter={this.state.type} data={Pokemons} />
+          </div>
+          <button onClick={this.setNext} className="next-pokemon" />
+          <button onClick={this.setPreviously} className="previously-pokemon" />
         </div>
-        <Card filter={this.state.type} data={Pokemons}/>
       </div>
     );
   }
 }
-
-export default App;
