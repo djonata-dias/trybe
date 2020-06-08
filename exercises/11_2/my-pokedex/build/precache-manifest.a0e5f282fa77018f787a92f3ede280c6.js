78b933ec3c46173af75036805892229df65c3118
djonata-dias <diasdjonata@gmail.com>
2020-06-08 18:50:06 -0300

    colocando exercicios react no repositorio

---
+++ exercises/11_2/my-pokedex/build/precache-manifest.a0e5f282fa77018f787a92f3ede280c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58ec9184c10ca0a613d146d7cb8267a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf9e65db2897699605e2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43919cc40289b43f0dc9",
+    "url": "/static/js/2.215ee01d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.215ee01d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf9e65db2897699605e2",
+    "url": "/static/js/main.2d60c3b5.chunk.js"
+  },
+  {
+    "revision": "e4f10e117a0adf087adf",
+    "url": "/static/js/runtime-main.c4a62e23.js"
+  }
+]);
---
+++ exercises/12_1/my-pokedex/build/precache-manifest.859ae3dd0a9bcd0aa2e30acd1a964b1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b635a50cf684a6c202ba05e28af476",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53ad536f7086a6bcde11",
+    "url": "/static/css/main.286e6c4b.chunk.css"
+  },
+  {
+    "revision": "82785506addde7140f40",
+    "url": "/static/js/2.bc97385c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc97385c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ad536f7086a6bcde11",
+    "url": "/static/js/main.6a00d0bf.chunk.js"
+  },
+  {
+    "revision": "8389353996b2b22286d5",
+    "url": "/static/js/runtime-main.3e61e05b.js"
+  },
+  {
+    "revision": "93769aee03dbdd550b972f59454c0299",
+    "url": "/static/media/pokedex-background.93769aee.jpg"
+  }
+]);
